8b2539f146c98c405fef80392912bab1c74f2388
Tema <124@mail.ru>
2023-02-06 11:49:01 +0300

    start

---
+++ Makefile
@@ -0,0 +1,6 @@
+publish: 
+		npm publish --dry-run
+install:
+		npm ci
+brain-games:
+		node bin/brain-games.js
---
+++ bin/brain-games.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const game = () => {
+    console.log('Welcome to the Brain Games!')
+};
+
+game()
